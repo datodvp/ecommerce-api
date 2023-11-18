0954cfe7099140219f83132d0e9a32387fa5f572
datodvp <gaguadato98@gmail.com>
2023-11-18 13:02:37 +0400

    feat: add mongodb connection

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB connection succesfull!'))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend server is running!');
+});
