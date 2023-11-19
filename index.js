eb256163e760b05cb00302bf1c7ab2352922d1ac
datodvp <gaguadato98@gmail.com>
2023-11-19 15:23:50 +0400

    feat: add checkout route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const checkoutRoute = require('./routes/checkout');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -15,12 +17,14 @@ mongoose
   .then(() => console.log('DB connection succesfull!'))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', checkoutRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!');
